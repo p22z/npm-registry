4764e81f1d5a198295ce4976e867cdad320cca95
p22z <github@p22z.com>
2026-01-21 15:55:11 +0100

    USDC Autheo Address

--- src/chains/testnet/785.js
+++ src/chains/testnet/785.js
@@ -6,7 +6,8 @@ const chainConfig = {
   "message": "0x19E5941DAB908c557eD68d12043acD4e93e640D5",
   "feeToken": "0xF69a96A44Ac1C65eCBB0869e990a409CD8f6ddB0",
   "weth": "0x7065Bca1c37924d2f5F90eDC5fd1D6D9227200d8",
-  "featureGateway": "0xbb3760307d1A5e81dE7639f01F7675EDfc86a773"
+  "featureGateway": "0xbb3760307d1A5e81dE7639f01F7675EDfc86a773",
+  "usdc": "0xcE850Eac51cAb95d886a756eeBE1Bd132bc4593e"
 };
 
 module.exports = chainConfig;
